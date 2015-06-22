e158f5c2fb4a0c67ad403a5465679d214f935875
Goomii <GoomiChan@users.noreply.github.com>
2015-06-22 13:27:17 +0100

    Some room template functions

--- server/room.js
+++ server/room.js
@@ -1,3 +1,22 @@
 module.exports = {
-	
+
+	// Create a new room
+	Create: function(roomID) {
+
+	},
+
+	// Get a room by its ID
+	Get: function(roomID) {
+
+	},
+
+	// Do stuff based on a message from a player
+	HandlePlayerMessage: function(playerData, Message) {
+
+	},
+
+	// Process logic for this room
+	Tick: function(deltaTime) {
+
+	}
 };
