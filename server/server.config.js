04b5592ded9a7ae795630e2d67aae0333adc2867
Kieran Harkin <k.harkin92@gmail.com>
2015-06-24 23:22:25 +0100

    silly merges

--- server/server.config.js
+++ server/server.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	Env: 'dev',
-	PublicDir: '../public',
-	AppDir: '../app/',
+	PublicDir: '../public/',
+	AppDir: '../public/app/',
 
 	SteamAPIKey: '548AE99AD2345B97F217E36C46894951',
 	
