0dfa696db5b74761d940e45b8ed429eef0693fd4
Kieran Harkin <k.harkin92@gmail.com>
2015-06-24 23:57:10 +0100

    seriously... notice me plz

--- server/server.config.js
+++ server/server.config.js
@@ -5,10 +5,10 @@ module.exports = {
 	Host: 'uat.ssg15.0x.tf',
 	Port: 8080,
 	Secure: false,
-	getUrl: function() { return (Secure ? 'https://' : 'http://')+Host+(Port != 80 ? ':'+Port+'/' : '/'); },
 		
 	SteamAPIKey: '548AE99AD2345B97F217E36C46894951',
 	
 	//Room settings
 	MaxPlayers: 10000
 };
+module.exports.getUrl = function() { return (module.exports.Secure ? 'https://' : 'http://')+module.exports.Host+(module.exports.Port != 80 ? ':'+module.exports.Port+'/' : '/'); };
