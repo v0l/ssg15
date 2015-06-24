83ebb32d7afdf5574365a3ae6795e7ba4b11a36d
Kieran Harkin <k.harkin92@gmail.com>
2015-06-24 23:54:04 +0100

    typo san

--- server/server.config.js
+++ server/server.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	Host: 'uat.ssg15.0x.tf',
 	Port: 8080,
 	Secure: false,
-	getUrl: function() { return (Secure ? 'https://' : 'http://')+Host+(Port != 80 : ':'+Port+'/' : '/'); }
+	getUrl: function() { return (Secure ? 'https://' : 'http://')+Host+(Port != 80 ? ':'+Port+'/' : '/'); }
 		
 	SteamAPIKey: '548AE99AD2345B97F217E36C46894951',
 	
